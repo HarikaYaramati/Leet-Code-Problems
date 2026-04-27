d7ff1db07bacff79cf347bfe7d6093a4658cf833
HarikaYaramati <harika.yaramati@gmail.com>
2026-04-27 20:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (22.98%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n<= 0 :
+            return False
+        while n % 2 == 0:
+            n = n//2 
+        if n == 1 :
+            return True
+        else :
+            return False 
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
