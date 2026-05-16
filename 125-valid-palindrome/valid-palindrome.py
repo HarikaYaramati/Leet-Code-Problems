ea7ee4b09284bf5139eca9fe278b69082d1dd143
HarikaYaramati <harika.yaramati@gmail.com>
2026-05-16 17:38:41 +0530

    Time: 3 ms (98.99%) | Memory: 19.8 MB (43.84%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        st = ''
+        s = s.lower()
+        for i in s:
+            if i in 'abcdefghijklmnopqrstuvwxyz0123456789':
+                st+=i
+        return st == st[::-1]
